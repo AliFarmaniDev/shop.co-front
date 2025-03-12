f2c50ab0776f838607a98125ccc765a00a79599f
alireza <alirezafarmani619@gmail.com>
2025-03-12 20:40:04 +0330

    create and set routes and pages

---
+++ src/views/CartView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>car view</h2>
+  </div>
+</template>
---
+++ src/views/CategoryView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h2>categoiry page</h2>
+  </div>
+</template>
